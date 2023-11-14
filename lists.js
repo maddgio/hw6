936d5281a89ca0f668897e2ba90291d91ce98a7b
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-11-14 10:25:06 -0600

    Create lists.js

---
+++ lists.js
@@ -0,0 +1,51 @@
+<div id="countries">
+
+  <input class="search" placeholder="Search" />
+  <button class="sort" data-sort="name">
+    Sort
+  </button>
+
+  <ul class="list">
+    <li>
+      <h3 class="name">India</h3>
+      <p class="population">1,428,627,663</p>
+    </li>
+    <li>
+      <h3 class="name">China</h3>
+      <p class="population">1,425,671,352</p>
+    </li>
+    <li>
+      <h3 class="name">United States</h3>
+      <p class="population">339,996,563	</p>
+    </li>
+    <li>
+      <h3 class="name">Indonesia</h3>
+      <p class="population">277,534,122</p>
+    </li>
+   <li>
+      <h3 class="name">Pakistan</h3>
+      <p class="population">240,485,658</p>
+    </li>
+   <li>
+      <h3 class="name">Nigeria</h3>
+      <p class="population">223,804,632</p>
+    </li>
+  <li>
+      <h3 class="name">Brazil</h3>
+      <p class="population">216,422,446</p>
+    </li>
+  <li>
+      <h3 class="name">Bangladesh</h3>
+      <p class="population">172,954,319</p>
+    </li>
+  <li>
+      <h3 class="name">Russia</h3>
+      <p class="population">144,444,359</p>
+    </li>
+  <li>
+      <h3 class="name">Mexico</h3>
+      <p class="population">128,455,567</p>
+    </li>
+  </ul>
+
+</div>
